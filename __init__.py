b0b6bda00e625fb628fc0839910059de510bed3e
JeroenvO <j.j.v.oorschot@tue.nl>
2020-11-02 22:57:33 +0100

    Update __init__.py
    Fix escaping in mqtt. Add unique id.

--- __init__.py
+++ __init__.py
@@ -1,33 +1,34 @@
  
-import touchpads, keypad, display, wifi, audio, time, system, appconfig
+import touchpads, keypad, display, wifi, audio, time, system, appconfig, machine
 from umqtt.simple import MQTTClient
-
+import binascii
 # Get the settings from the settings menu
-settings = appconfig.get('mqtt_button', {'MQTT_server_ip': "Your IP", 
-                                         'MQTT_device_name': "/cz2020",
+settings = appconfig.get('mqtt_button', {'MQTT_server_ip': "192.168.1.104", 
+                                         'MQTT_device_name': "cz2020",
                                          'MQTT_discovery_prefix': 'homeassistant' })
 SERVER_IP = settings['MQTT_server_ip']
-DEVICE_NAME = settings['MQTT_device_name']+"/"
-PREFIX = settings['MQTT_discovery_prefix']+"/" # todo fix that the all / are correct
-DEVICE_CONFIG = {
-    "identifiers": "5ccf7f0f4ff3",
-    "name": DEVICE_NAME,
-    "sw_version": "cz2020-badge.team v0.1",
-    "model": "CZ2020",
-    "manufacturer": "badge.team"
-  }
+DEVICE_NAME = settings['MQTT_device_name'].strip('/')
+PREFIX = settings['MQTT_discovery_prefix'].strip('/')
+UUID = binascii.hexlify(machine.unique_id()).decode()
+DEVICE_CONFIG = '{\
+    "identifiers": "'+UUID+'",\
+    "name": "'+DEVICE_NAME+'",\
+    "sw_version": "cz2020-badge.team v0.1",\
+    "model": "CZ2020",\
+    "manufacturer": "badge.team"\
+  }'
 COLORS = [int(0xff0000)]*16   # save color for each button.
 
 # Clear the screen
-display.drawFill(0x000000)
+display.drawFill(0xffffff)
 display.flush()
 
 # Key press handler
 def on_key(key_index, pressed):
-    global COLORS
+    global COLORS, DEVICE_NAME
     x, y = key_index % 4, int(key_index / 4)
     print('key event',key_index)
-    topic = PREFIX+'binary_sensor'+DEVICE_NAME+str(key_index)+'/state'
+    topic = PREFIX+'/binary_sensor/'+DEVICE_NAME+'/'+str(key_index)+'/state'
     if(pressed):
         c.publish(topic, "on")
         display.drawPixel(x, y, 0x00FF00)
@@ -89,9 +90,9 @@ try:
     # Subscibe to the "led" topic
     #c.subscribe(TOPIC+"led/#")  # todo mqtt light
     for key_index in range(16):  # each button
-        topic = PREFIX+'binary_sensor'+DEVICE_NAME+str(key_index)+'/'
+        topic = PREFIX+'/binary_sensor/'+DEVICE_NAME+'/'+str(key_index)+'/'
         c.publish(topic+"config", 
-                  {"name": "cz20-btn"+str(key_index), "state_topic": topic+'state', "unique_id":DEVICE_NAME+'-btn'+str(key_index), "device":DEVICE_CONFIG})
+                  '{"name": "cz20-btn'+str(key_index)+'", "state_topic":"'+topic+'state", "unique_id":"'+DEVICE_NAME+'-btn'+str(key_index)+'", "device":'+DEVICE_CONFIG+'}')
 except:
     audio.play('/apps/mqtt_button/fail_connect_mqtt.mp3')
     time.sleep(6)
@@ -106,3 +107,4 @@ try:
             c.check_msg()
 finally:
     c.disconnect()
+    
