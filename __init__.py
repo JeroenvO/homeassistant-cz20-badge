664b5a0acf0f482639fde15414b725e70bc07df7
jeroenvo <j.j.v.oorschot@tue.nl>
2020-11-08 14:42:50 +0100

    Trying multiple instances simultaneous

--- __init__.py
+++ __init__.py
@@ -13,23 +13,27 @@ import binascii
 # Get the settings from the settings menu
 settings = appconfig.get('homeassistant_cz20_badge', {'MQTT_server_ip': "192.168.1.104",
                                                       'MQTT_device_name': "cz2020",
-                                                      'MQTT_discovery_prefix': 'homeassistant'})
+                                                      'MQTT_discovery_prefix': 'homeassistant',
+                                                      'on_press_color': '0xffffff'})
 SERVER_IP = settings['MQTT_server_ip']
 DEVICE_NAME = settings['MQTT_device_name'].strip('/')
 PREFIX = settings['MQTT_discovery_prefix'].strip('/')
+ON_PRESS_COLOR = int(settings['on_press_color'].strip(), 16)
 UUID = binascii.hexlify(machine.unique_id()).decode()
+NODE_ID = UUID
 DEVICE_CONFIG = '{\
 "identifiers": "' + UUID + '",\
 "name": "' + DEVICE_NAME + '",\
 "sw_version": "cz2020-badge.team v1",\
-"model": "CZ2020",\
+"model": "CampZone 2020 badge",\
 "manufacturer": "badge.team and JeroenvO"\
 }'
+
 ORANGE = 0xFF5500
 RED = 0xff0000
 GREEN = 0x00ff00
-COLORS = [[255, 255, 255]] * 16  # save color, brightness for each button.
-BRIGHTNESS = [255] * 16  # save brightness for each button.
+COLORS = [[0, 0, 0]] * 16  # save color, brightness for each button.
+BRIGHTNESS = [0] * 16  # save brightness for each button.
 STATE = [False] * 16  # save state for each button.
 
 # Clear the screen
@@ -43,7 +47,7 @@ def set_color(key_index, on=True):
         cs = [int(c * BRIGHTNESS[key_index] / 255) for c in COLORS[key_index]]
         display.drawPixel(x, y, (cs[0] << 16) + (cs[1] << 8) + cs[2])
     else:
-        display.drawPixel(x, y, 0)
+        display.drawPixel(x, y, ON_PRESS_COLOR)
     display.flush()
 
 
@@ -51,11 +55,11 @@ def set_color(key_index, on=True):
 def on_key(key_index, pressed):
     global c
     # print('key event', key_index)
-    topic = PREFIX + '/binary_sensor/' + DEVICE_NAME + '/' + str(key_index) + '/state'
+    topic = PREFIX + '/binary_sensor/' + NODE_ID + '/' + str(key_index) + '/state'
     if pressed:
         x, y = key_index % 4, int(key_index / 4)
         c.publish(topic, "ON")
-        display.drawPixel(x, y, RED)
+        display.drawPixel(x, y, 0xffffff)  # bright white on press
         display.flush()
     else:
         c.publish(topic, "OFF")
@@ -70,9 +74,9 @@ def on_home(is_pressed):
         display.drawFill(RED)
         display.flush()
         for key_index in range(16):  # each button
-            topic = PREFIX + '/binary_sensor/' + DEVICE_NAME + '/' + str(key_index) + '/'
+            topic = PREFIX + '/binary_sensor/' + NODE_ID + '/' + str(key_index) + '/'
             c.publish(topic + "status", "offline")
-            topic = PREFIX + '/light/' + DEVICE_NAME + '/' + str(key_index) + '/'
+            topic = PREFIX + '/light/' + NODE_ID + '/' + str(key_index) + '/'
             c.publish(topic + "status", "offline")
         c.disconnect()
         system.launcher()
@@ -87,7 +91,7 @@ def sub_cb(topic, msg):
     topic = topic.decode('utf-8').split('/')
     key_index = int(topic[3])
     command = topic[4]
-    topic_u = PREFIX + '/light/' + DEVICE_NAME + '/' + str(key_index) + '/'
+    topic_u = PREFIX + '/light/' + NODE_ID + '/' + str(key_index) + '/'
     msg = msg.decode('utf-8')
     if command == 'switch':
         if msg == 'ON':
@@ -123,7 +127,7 @@ if not wifi.status():
         display.flush()
         time.sleep(1)
         system.launcher()
-display.drawLine(0, 0, 3, 0, 0x00ff00)  # wifi success
+display.drawLine(0, 0, 3, 0, GREEN)  # wifi success
 display.flush()
 
 # Setup MQTT and connect to it
@@ -133,9 +137,9 @@ c = MQTTClient("umqtt_client", SERVER_IP)
 c.set_callback(sub_cb)
 # Set a last-will
 for key_index in range(16):  # each button
-    topic = PREFIX + '/binary_sensor/' + DEVICE_NAME + '/' + str(key_index) + '/'
+    topic = PREFIX + '/binary_sensor/' + NODE_ID + '/' + str(key_index) + '/'
     c.set_last_will(topic + "status", "offline", retain=True)
-    topic = PREFIX + '/light/' + DEVICE_NAME + '/' + str(key_index) + '/'
+    topic = PREFIX + '/light/' + NODE_ID + '/' + str(key_index) + '/'
     c.set_last_will(topic + "status", "offline")
 
 # Connect to MQTT server, fail if not possible and return.
@@ -146,17 +150,17 @@ if c.connect():
     time.sleep(1)
     system.launcher()
 
-display.drawLine(0, 1, 3, 1, 0x00ff00)  # mqtt success part 1
+display.drawLine(0, 1, 3, 1, GREEN)  # mqtt success part 1
 display.drawLine(0, 2, 3, 2, ORANGE)  # mqtt start part 2
 display.flush()
 
 for key_index in range(16):  # each button
-    topic = PREFIX + '/binary_sensor/' + DEVICE_NAME + '/' + str(key_index) + '/'
+    topic = PREFIX + '/binary_sensor/' + NODE_ID + '/' + str(key_index) + '/'
     message = '{' + '"name": "btn-{key_index:02d}", "state_topic":"{topic}state", "avty_t":"{topic}status", "unique_id":"{UUID}-btn{key_index}", "device":{DEVICE_CONFIG}'.format(
         key_index=key_index, topic=topic, UUID=UUID, DEVICE_CONFIG=DEVICE_CONFIG) + '}'
     c.publish(topic + "config", message)
     c.publish(topic + "status", "online")
-    topic = PREFIX + '/light/' + DEVICE_NAME + '/' + str(key_index) + '/'
+    topic = PREFIX + '/light/' + NODE_ID + '/' + str(key_index) + '/'
     message = '{' + \
               '"name": "btn-{key_index:02d}-light","state_topic":"{topic}state","avty_t":"{topic}status","command_topic":"{topic}switch", "brightness_state_topic":"{topic}brightness/state","brightness_command_topic":"{topic}brightness/set","rgb_state_topic":"{topic}rgb/state","rgb_command_topic":"{topic}rgb/set", "unique_id":"{UUID}-btn{key_index}-light", "device":{DEVICE_CONFIG}, "retain":true'. \
                   format(key_index=key_index, topic=topic, UUID=UUID, DEVICE_CONFIG=DEVICE_CONFIG) + '}'
@@ -167,7 +171,7 @@ display.drawLine(0, 2, 3, 2, 0x00ff00)  # mqtt finish part 2
 display.drawLine(0, 3, 3, 3, ORANGE)  # mqtt part 3
 display.flush()
 
-topic = PREFIX + '/light/' + DEVICE_NAME + '/'
+topic = PREFIX + '/light/' + NODE_ID + '/'
 c.subscribe(topic + "#")
 
 display.drawLine(0, 3, 3, 3, GREEN)  # mqtt finish part 2
@@ -176,6 +180,9 @@ display.flush()
 # Configure the key press handler
 keypad.add_handler(on_key)
 touchpads.on(touchpads.HOME, on_home)
+
+display.drawFill(0x000000)
+display.flush()
 
 try:
     while 1:
